d7ec440ff1f02d073dadb4e4653291d0f008073c
adminsfmeal <42289953+adminsfmeal@users.noreply.github.com>
2023-01-13 14:47:47 -0800

    Add about page & contact us page

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -1,14 +1,200 @@
 import { MetaTags } from '@redwoodjs/web'
 
+import StyleHeader from 'src/components/StyleHeader/StyleHeader'
+import dongwu from 'src/images/dongwu.jpeg'
+import emotion1 from 'src/images/emotion1.jpeg'
+import food1 from 'src/images/food1.jpeg'
+import food2 from 'src/images/food2.jpeg'
+import me from 'src/images/me.jpeg'
+
+const meBGStyle = {
+  backgroundImage: 'url(' + me + ')',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: '100%',
+  backgroundPosition: 'center',
+}
+
+const dongwuBGStyle = {
+  backgroundImage: 'url(' + dongwu + ')',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: '100%',
+  backgroundPosition: 'center',
+}
+
+const food1BGStyle = {
+  backgroundImage: 'url(' + food1 + ')',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: '100%',
+  backgroundPosition: 'center',
+}
+
+const food2BGStyle = {
+  backgroundImage: 'url(' + food2 + ')',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: '100%',
+  backgroundPosition: 'center',
+}
+
+const emotion1BGStyle = {
+  backgroundImage: 'url(' + emotion1 + ')',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: '100%',
+  backgroundPosition: 'center',
+}
+
 const AboutPage = () => {
   return (
     <>
       <MetaTags title="About" description="About page" />
+      <div className="p-4 -mt-8">
+        <StyleHeader dark={false} />
+      </div>
 
-      <p className="font-light">
-        This site was created to demonstrate my mastery of Redwood: Look on my
-        works, ye mighty, and despair!
-      </p>
+      <div className="flex flex-row flex-wrap lg:flex-nowrap text-center text-gray-900">
+        <div className="basis-full lg:basis-1/3">
+          <p>关于我</p>
+          <div
+            className="rounded-full w-32 h-32 mx-auto mt-4"
+            style={meBGStyle}
+          />
+          <p className="pt-4 leading-8 px-12">
+            <span className="underline underline-offset-8">
+              一切都是最好的安排
+            </span>
+            <p className="pt-8 text-left">
+              2011年毕业于上海海事大学计算机专业, 毕业后从事网页游戏前段开发,
+              参与开发了一款月流水千万级别的游戏,
+              在页游市场井喷的第一时间上了车,
+              分得一波红利的同时也见证了国内产品市场的竞争。
+              3个月开发一款非换皮游戏, 开发出来不好玩, 然后策划连夜出版本,
+              接着又是1个月的挑灯夜码, 游戏的成功少不了速度的功劳,
+              背后是一个个早上7,8点的太阳, 是早上回家的时候能见到7,8点的太阳。
+              毕业后第一份工作让我感受到了市场的红利, 也让我体验到了加班的阴影,
+              也让我体会到资本市场的残酷。我选择了辞职来到了美国加州旧金山,
+              我在硅谷孵化器做过IOS开发, 在Catering初创做过送餐,
+              电商平台卖过汝窑茶杯, 最终在找SDE工作之前决定创业。
+              <p className="pt-4 underline underline-offset-8">
+                科技融于衣食住行
+              </p>
+              <p className="pt-4 text-left">
+                第一个创业的项目是私房菜送餐平台,
+                是一个连接私房菜厨师和用户的基于中央厨房的点餐送餐平台。有这个想法是因为在朋友圈有很多在微信上卖吃的的人,
+                然后因为家庭厨房的环境条件,
+                很多都不符合规定而且也很难形成规模效应。我当时一直在从事catering的送餐工作,
+                每天都会从不同的商业厨房取餐送到公司,
+                这个过程了解到商业厨房其实并不神秘而且成本对比起动辄上百人到五百人的餐量来说实在是太低了。
+              </p>
+              <p className="pt-4 text-left">
+                一个想法油然而生, 那我为何不创建一个平台, 把私厨资源集合在一起,
+                通过聚合私厨吸引用户订单,用preorder的形式提高私厨的厨房利用率。
+                简单来说, 用户可以用更低的价钱吃到有特色的小吃。
+                为了吸引第一批用户, 我去上海和成都学习了全国各地的小吃,
+                成为了平台的第一批厨师, 小龙虾, 麻辣干锅, 千里香小馄饨,
+                老鸭粉丝汤, 重庆酸辣粉也成为了平台的流量密码。
+                疫情是我们的红利期, 但是产品逐渐成为了瓶颈,
+                用户留存一直提不上去, 每天还要炒菜, 宣传和送餐,
+                顾此失彼导致了产品一直没时间优化,
+                团队管理经验的缺失导致了这次创业的失败。
+                结束第一个创业平台的契机是我开了一家烧烤店。
+              </p>
+              <p className="pt-4 underline underline-offset-8">团队的力量</p>
+              <p className="pt-4 text-left">
+                烧烤店的经历第一次让我感受最深的是团队的力量,
+                我和合伙人从2021年6月开始接手,
+                从学如何调撒料开始到把月流水做到每月15-20%的增长的同时还能控制支出.
+                短短的半年时间, 我学会了流程优化, 定制KPI,
+                激励员工和如何异步高效合作。 每一步都是团队工作,
+                少不了沟通交流和分工, 最终结果就是我们做到了三番第一,
+                荣获吃货小分队2021度湾区最受欢迎烧烤店的称号。
+                这一次的经历让我第一次真正在团队中成长,
+                让我坚信我有能力自己带团队,而且在团队分工中可以做更好的自己。
+              </p>
+              <p className="pt-4 underline underline-offset-8">
+                再一次原地出发, 只是离月光更近了
+              </p>
+              <p className="pt-4 pb-12">下面就是洞屋剧本杀的前世今生了...</p>
+            </p>
+          </p>
+        </div>
+        <div className="basis-full lg:basis-1/3">
+          <p className="text-lg font-light pt-8 lg:pt-0">Short Story</p>
+          <p className="pt-4 underline underline-offset-8">最长的电影</p>
+          <p className="pt-4 px-8 leading-8 text-left">
+            我们的开始, 是最长的电影。放映了三年, 我票都还留着。
+            这句歌词用来形容选对本的体验再好不过了。
+            <br />
+            <br />
+            洞屋剧本杀会了解你的打本偏好, 记录你这个世界的人设,
+            了解你内心深处最脆弱的部分。 目的只有一个, 让你拿到一个本看完背景,
+            有这样一种感觉: “哇, 这个人好像是专门为我而设的” 或者
+            ”我们明明活在两个千差万别的世界, 可是感觉好熟悉啊“
+            <div
+              className="rounded w-64 h-48 mx-auto my-4"
+              style={dongwuBGStyle}
+            />
+            <p className="pt-4 underline underline-offset-8 text-center">
+              唯美食与爱不可辜负
+            </p>
+            <p className="pt-4">
+              更多时候, 在我看来, 美食是一种回忆, 是一种期待,
+              是一种羁绊也是一种画面。 洞屋剧本杀把美食融入进了剧本杀之中,
+              给大家另一段人生增加一份开始前的期许, 就像病娇男孩对蛋糕的期待,
+              一份人生中必不可少的调味剂, 正如神乐汤中上“神乐汤”时的五味杂陈,{' '}
+              和一份让你津津乐道的回忆,
+              就像作者不详中对“冷串串”恐怖又期待的记忆。
+              <br />
+              <br />
+              欢迎你们, 和我一起体验鲜活的人生。
+            </p>
+            <div
+              className="rounded-md w-72 h-48 mx-auto my-4"
+              style={food1BGStyle}
+            />
+            <p className="pt-4">
+              漓川怪谈薄之中, 小女孩作为一个边缘人物, 被我们做成了NPC,
+              当她拿出妖灵冰粉时, 说一句欢迎来到“漓川”的时候,
+              那种惟妙惟肖的神情在她端着冰粉那一刻得到了延续,光怪陆离的景象仿佛在冰粉上映射出来。
+            </p>
+            <div
+              className="rounded-md w-80 h-56 mx-auto my-4"
+              style={food2BGStyle}
+            />
+          </p>
+        </div>
+        <div className="basis-full lg:basis-1/3">
+          <p className="text-lg font-light">Long Story</p>
+          <p className="pt-4 px-8 leading-8 text-left">
+            《那一束月光》的吴倩尹的玩家, 因为故事过于代入,
+            几天之后还在emo。她也对情感本改观了, 我无法窥探她的心思,
+            不过相信她会觉得这是一段自己人生的延续,
+            不管结果如何是一段美好的体验, 难以忘怀。 突然想到一句话,
+            选对本就像体验了一部最长的电影, 久久不想谢幕。
+            选错本就像替别人蹲了个牢, 无期徒刑的那种。 <br />
+            <br />
+            剧本杀是个社交游戏, 需要代入剧本, 需要和玩家对话,
+            好的剧本打完甚至几天都未能走出剧本,
+            所以我创立洞屋剧本杀的初衷是除了了解你剧中人物的匹配度,
+            还想仔细聆听你剧本前世的人生, 也就是你的个人偏好,
+            一切都是最好的安排, 我想让你拿到这个本看完背景, 有这样一种感觉: “哇,
+            这个人好像是专门为我而设的” 或者 ”我们明明活在两个千差万别的世界,
+            可是感觉好熟悉啊“
+            <div
+              className="rounded w-48 h-64 mx-auto my-4"
+              style={emotion1BGStyle}
+            />
+            要做到这点,能让我们提前在茫茫剧本中为玩家初选出适合他/她的剧本和角色,
+            毕竟这是一场好几个小时的人生体验, 谁也不想做菠萝头和坐牢的吧,
+            我也不希望这个锅只能是玩家自己背。要做到让玩家对我们敞开心扉,
+            也不是容易的, 我记得一名打过 《那一束月光》的女嘉宾说:
+            “我提议你打电话去问那个选角问题, 因为有些答案不是文字可以描述的,
+            沉默, 停顿, 哽咽, 甚至说话的语气, 是悲愤, 是冷静, 还是义愤填膺”
+            确实, 透过冰冷的文字无法完全感受回答人的心情,
+            就像无法对一个完全的陌生人敞开心扉一样. 剧本杀出现之前,
+            这像一个永远的矛盾题, 你不对我敞开心扉我就不能理解你,
+            你不理解我我怎么敢给你看我内心最脆弱的部分。
+          </p>
+        </div>
+      </div>
     </>
   )
 }
--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,4 +1,5 @@
-import { MetaTags } from '@redwoodjs/web'
+import { useState } from 'react'
+
 import {
   Form,
   TextField,
@@ -7,10 +8,29 @@ import {
   FieldError,
   Label,
   FormError,
+  SelectField,
   useForm,
 } from '@redwoodjs/forms'
+import { MetaTags } from '@redwoodjs/web'
 import { useMutation } from '@redwoodjs/web'
 import { toast, Toaster } from '@redwoodjs/web/toast'
+
+import StyleHeader from 'src/components/StyleHeader/StyleHeader'
+import qrcode from 'src/images/qrcode.png'
+
+const qrcodeBGStyle = {
+  backgroundImage: 'url(' + qrcode + ')',
+  backgroundRepeat: 'no-repeat',
+  backgroundSize: '100%',
+  backgroundPosition: 'center',
+}
+
+const backgroundImage = {
+  backgroundImage:
+    'url(' +
+    'https://images.unsplash.com/photo-1536300007881-7e482242baa5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1920&q=80' +
+    ')',
+}
 
 const CREATE_CONTACT = gql`
   mutation CreateContactMutation($input: CreateContactInput!) {
@@ -22,6 +42,7 @@ const CREATE_CONTACT = gql`
 
 const ContactPage = () => {
   const formMethods = useForm()
+  const [questionIndex, setQuestionIndex] = useState(0)
 
   const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
     onCompleted: () => {
@@ -35,81 +56,188 @@ const ContactPage = () => {
     console.log(data)
   }
 
+  const onQuestionChange = (value) => {
+    console.log(value.target.value)
+    setQuestionIndex(value.target.value)
+  }
+
   return (
     <>
       <MetaTags title="Contact" description="Contact page" />
 
       <Toaster />
-      <Form
-        onSubmit={onSubmit}
-        config={{ mode: 'onBlur' }}
-        error={error}
-        formMethods={formMethods}
-      >
-        <FormError
+
+      <div className="p-8">
+        <div
+          className="relative -mx-8 -mt-8 bg-center bg-cover"
+          style={backgroundImage}
+        >
+          <div className="absolute inset-0 bg-black opacity-75"></div>
+          <div className="relative px-6 sm:px-8 mx-auto h-full flex flex-col">
+            <StyleHeader isHome={true} dark={true}></StyleHeader>
+            <div className="px-4 flex flex-1 pb-32">
+              <h1 className="text-3xl text-left sm:text-4xl lg:text-5xl xl:text-6xl font-black text-gray-100 leading-snug sm:mt-0 pt-8">
+                <span className="inline-block mt-2">
+                  欢迎你, 剧本人!
+                  <br />
+                  <p className="text-sm mt-8">
+                    无论你是对推理感兴趣的新手玩家,
+                    还是对剧本杀入迷想寻找固定车队的玩家,
+                    还是百本玩家想寻找不同的打本体验的
+                    <br />
+                  </p>
+                  <p className="text-sm mt-8 underline cursor-pointer">
+                    有任何问题想问我们的?
+                  </p>
+                </span>
+              </h1>
+            </div>
+          </div>
+        </div>
+
+        <Form
+          onSubmit={onSubmit}
+          config={{ mode: 'onBlur' }}
+          className="-mt-8 text-center"
           error={error}
-          wrapperClassName="py-4 px-6 rounded-lg bg-red-100 text-red-700"
-          listClassName="list-disc ml-4"
-          listItemClassName=""
-        />
-        <Label
-          name="name"
-          className="block text-gray-700 uppercase text-sm"
-          errorClassName="block uppercase text-sm text-red-700"
+          formMethods={formMethods}
         >
-          Name
-        </Label>
-        <TextField
-          name="name"
-          validation={{ required: true }}
-          className="border rounded-sm px-2 py-1 outline-none"
-          errorClassName="border rounded-sm px-2 py-1 border-red-700 outline-none"
-        />
-        <FieldError name="name" className="block text-red-700" />
+          <FormError
+            error={error}
+            wrapperClassName="py-4 px-6 rounded-lg bg-red-100 text-red-700"
+            listClassName="list-disc ml-4"
+            listItemClassName=""
+          />
 
-        <Label
-          name="email"
-          className="block mt-8 text-gray-700 uppercase text-sm"
-          errorClassName="block mt-8 text-red-700 uppercase text-sm"
-        >
-          Email
-        </Label>
-        <TextField
-          name="email"
-          validation={{
-            required: true,
-            pattern: {
-              value: /[^@]+@[^.]+\..+/,
-              message: 'Please enter a valid email address',
-            },
-          }}
-          className="border rounded-sm px-2 py-1"
-          errorClassName="border rounded-sm px-2 py-1 border-red-700 outline-none"
-        />
-        <FieldError name="email" className="block text-red-700" />
+          <SelectField
+            name="question"
+            className="border rounded-lg py-4 px-4 md:w-96 w-full drop-shadow-lg"
+            onChange={onQuestionChange}
+          >
+            <option className=" text-gray-200">Choose Category</option>
+            <option value="1">新人入坑指南</option>
+            <option value="2">洞屋剧本杀特色</option>
+            <option value="3">寻找固定车队</option>
+            <option value="4">美食剧本杀是什么</option>
+            <option value="5">联系客服</option>
+          </SelectField>
+        </Form>
+      </div>
 
-        <Label
-          name="message"
-          className="block mt-8 text-gray-700 uppercase text-sm"
-          errorClassName="block mt-8 text-red-700 uppercase text-sm"
-        >
-          Message
-        </Label>
-        <TextAreaField
-          name="message"
-          validation={{ required: true }}
-          className="block border rounded-sm px-2 py-1"
-          errorClassName="block border rounded-sm px-2 py-1 border-red-700 outline-none"
-        />
-        <FieldError name="message" className="block text-red-700" />
+      <div className="px-16 py-8">
+        {questionIndex == 1 && (
+          <div className="flex-1">
+            <div>
+              <p className="font-bold">
+                Q: 没怎么玩过剧本杀, 怕动辄五六个小时的游戏会坐牢?
+              </p>
+              <p className=" font-light text-gray-600 pt-4 text-md">
+                不要担心, 我们有新人友好的剧本。在此之前,
+                我们相信你对推理有一定的兴趣,
+                也有接触过剧本杀的一些规则。所以我们会根据你喜欢的类型给你推荐新人友好本,
+                比如: 秒杀(欢乐), 情感(再见萤火虫), 惊悚(古木吟)
+                希望让你打一次就迷上剧本这个世界, 我们比别人多一个世界!
+              </p>
 
-        <Submit
-          className="block bg-blue-700 text-white mt-8 px-4 py-2 rounded"
-          disabled={loading}
-        >
-          Save
-        </Submit>
-      </Form>
+              <p className="font-bold pt-12">
+                Q: 我一拿到凶手就紧张, 话都不会说了怎么办?
+              </p>
+              <p className=" font-light text-gray-600 pt-4">
+                如果你担心拿到凶手会被盘状态盘出来,
+                可以提前私下告诉我们的主持人(DM), DM会尽量不给你凶手本的。
+                现在大部分的本子凶手都不知道自己是凶手的,
+                所以没有什么可以隐瞒的, 大可不必紧张。 最后, 万一知道自己是凶手,
+                但又顺利逃脱了呢? 那你的体验感就是成倍上升了。
+              </p>
+
+              <p className="font-bold pt-12">
+                Q: 我没玩过剧本杀, 怎么预约呢? 是不是需要找很多人一起?
+              </p>
+              <p className=" font-light text-gray-600 pt-4">
+                一个人也可以发车/上车的, 只需要加入我们的拼车群,
+                可以查看拼车信息。 或者联系客服发一车你感兴趣的剧本, 告诉时间,
+                剧本就可以开车了。 如果你有朋友也感兴趣,
+                一起开车或者整车来组队可以更容易发车哦! 约车拼车,
+                更多问题欢迎骚扰客服:
+              </p>
+
+              <div className=" w-56 h-64 mx-auto mt-4" style={qrcodeBGStyle} />
+            </div>
+          </div>
+        )}
+        {questionIndex == 2 && (
+          <div className="flex-1">
+            <div>
+              <p className="font-bold">
+                Q: 洞屋剧本杀和别的剧本杀店有什么不同?
+              </p>
+              <p className=" font-light text-gray-600 pt-4 text-md">
+                我们把美食和剧本结合在了一起, 目前有12个特别设计的美食本,
+                我们希望通过美食, 给大家的剧本人生增加一份期待,
+                一种调味剂和一份让你津津乐道的回忆。
+                每局我们为本场的MVP玩家设计了专属MVP卡片, 记录了你的打本偏向,
+                情感羁绊和个人人设, 这样我们就能更好地了解你, 为你选适合你的本。
+              </p>
+
+              <p className="font-bold pt-12">Q: 订剧本的时候怎么预定吃的?</p>
+              <p className=" font-light text-gray-600 pt-4">
+                预定剧本的时候DM会询问你们是否要预定配套美食, 美食按位计算价格,
+                价格从$6至$15不等。(配套美食只能提前预定, 不能临时点餐)
+              </p>
+
+              <p className="font-bold pt-12">Q: 可以不点配套餐饮吗?</p>
+              <p className=" font-light text-gray-600 pt-4">
+                配套餐饮是可选的, 为了你的完整打本体验我们建议尝试一下,
+                但如果你吃饱了或者想点外卖都是欢迎的。
+              </p>
+
+              <div className=" w-56 h-64 mx-auto mt-4" style={qrcodeBGStyle} />
+            </div>
+          </div>
+        )}
+        {questionIndex == 3 && (
+          <div className="flex-1">
+            <div>
+              <p className="font-bold">Q: 洞屋剧本杀还能帮我们寻找固定车队?</p>
+              <p className=" font-light text-gray-600 pt-4 text-md">
+                是的!我们会根据会员信息匹配出与你打本偏向一致的玩家,
+                还会根据玩家人设匹配出与你人设互补的玩家, 推荐给你!
+                只需要在会员页面填写个人资料(包括打本偏好, 人设和情感羁绊)
+              </p>
+
+              <p className="font-bold pt-12">Q: 如何寻找固定车队?</p>
+              <p className=" font-light text-gray-600 pt-4">
+                填好个人打本偏好后, 加客服微信, 发个人页面给客服,
+                客服通过后台会拉一个意向群, 里面都是偏好一致,
+                人设互补的小伙伴哦! 可以在群里自行组队。
+              </p>
+
+              <div className=" w-56 h-64 mx-auto mt-4" style={qrcodeBGStyle} />
+            </div>
+          </div>
+        )}
+        {questionIndex == 4 && (
+          <div className="flex-1">
+            <div>
+              <p className="font-bold">Q: 什么是美食剧本杀?</p>
+              <p className=" font-light text-gray-600 pt-4 text-md">
+                美食剧本杀是把美食融入到剧本杀的流程之中,
+                与传统的剧本杀+外卖是分开的不同,
+                剧本杀过程中的美食不仅是一种补充体力脑力的东西,
+                还是剧本杀的道具, 可以为剧本增香添彩,
+                上菜的NPC也会有相应的性格和角色, 不会跳戏。
+                其中的美食也是天南地北的特色小吃, 比如: 乐山冷串串, 麻辣干锅,
+                老鸭粉丝汤, 重庆伤心酸辣粉, 千里香小馄饨等等。
+              </p>
+
+              <div className=" w-56 h-64 mx-auto mt-4" style={qrcodeBGStyle} />
+            </div>
+          </div>
+        )}
+        {questionIndex == 5 && (
+          <div className=" w-56 h-64 mx-auto mt-4" style={qrcodeBGStyle} />
+        )}
+      </div>
     </>
   )
 }
