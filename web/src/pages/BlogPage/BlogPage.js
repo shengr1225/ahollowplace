d73f09cc73f57bbcd5d37e2fca1048d0be633ebc
adminsfmeal <42289953+adminsfmeal@users.noreply.github.com>
2023-01-29 17:06:21 -0800

    try to use json file for Netlify CMS

--- web/src/pages/BlogPage/BlogPage.js
+++ web/src/pages/BlogPage/BlogPage.js
@@ -5,21 +5,35 @@ import remarkGfm from 'remark-gfm'
 
 import { MetaTags } from '@redwoodjs/web'
 
+import StyleHeader from 'src/components/header/header'
+
 const BlogPage = () => {
   const [content, setContent] = useState('')
   useEffect(() => {
     fetch('/admin/blog/2023-01-26-my-first-blog.md')
       .then((r) => r.text())
       .then((text) => {
+        let info = getHeader(text, '---')
+        console.log(info)
         setContent(text)
       })
   })
 
+  let getHeader = (str, signal) => {
+    let firstIdx = str.indexOf(signal) + signal.length
+    let secondIdx = str.indexOf(signal, firstIdx)
+    return str.substring(firstIdx, secondIdx)
+  }
+
   return (
     <>
-      <MetaTags title="Blog" description="Blog page" />
-      <h1>剧本博客</h1>
-      <ReactMarkDown remarkPlugins={[remarkGfm]}>{content}</ReactMarkDown>
+      <MetaTags title="洞屋最新" description="洞屋动态|博客|随想|打本心得" />
+      <div className="p-4 -mt-8">
+        <StyleHeader dark={false} isHome={true} />
+      </div>
+      <div className="px-8 py-4 blog">
+        <ReactMarkDown remarkPlugins={[remarkGfm]}>{content}</ReactMarkDown>
+      </div>
     </>
   )
 }
