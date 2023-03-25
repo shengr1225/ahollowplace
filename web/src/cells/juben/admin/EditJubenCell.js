ecc631980089850a9b19b293a8adba3aba28b3ac
adminsfmeal <42289953+adminsfmeal@users.noreply.github.com>
2023-03-25 16:37:40 -0700

    fix a cell error in EditJubenCell

--- web/src/cells/juben/admin/EditJubenCell.js
+++ web/src/cells/juben/admin/EditJubenCell.js
@@ -3,6 +3,31 @@ import { toast } from '@redwoodjs/web/toast'
 
 import JubenForm from '../../../components/juben/JubenForm'
 
+export const QUERY = gql`
+  query EditJubenById($id: Int!) {
+    juben: juben(id: $id) {
+      id
+      name
+      score
+      image
+      desc
+      section
+      sections
+      players
+      canSwitchSex
+      duration
+      price
+      mvps {
+        id
+      }
+      timeSlots {
+        id
+      }
+      photos
+      available
+    }
+  }
+`
 const UPDATE_JUBEN_MUTATION = gql`
   mutation UpdateJubenMutation($id: Int!, $input: UpdateJubenInput!) {
     updateJuben(id: $id, input: $input) {
